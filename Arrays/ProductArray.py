29e3cb3b60e004b7704ca65ee8fca7acab14c128
Hameez Rizwan <hameexr@gmail.com>
2023-05-24 21:11:55 +0500

    fixed

--- Arrays/ProductArray.py
+++ Arrays/ProductArray.py
@@ -14,3 +14,25 @@
 #             filteredList = [num for num in nums if num != numberToFilter]
 #             result.append(Solution.multiply_array(filteredList))
 #         return result
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left_products = [1] * n
+        right_products = [1] * n
+        result = [1] * n
+        
+        # Calculate the product of all elements to the left of each element
+        for i in range(1, n):
+            left_products[i] = left_products[i-1] * nums[i-1]
+        
+        # Calculate the product of all elements to the right of each element
+        for i in range(n-2, -1, -1):
+            right_products[i] = right_products[i+1] * nums[i+1]
+            # print (right_products)
+        
+        # Multiply the corresponding left and right products to get the answer
+        for i in range(n):
+            result[i] = left_products[i] * right_products[i]
+        
+        return result
