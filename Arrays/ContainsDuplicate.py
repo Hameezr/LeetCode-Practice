72d6957c7a28ad68c6caa05c6f32f41b936e6dc7
Hameez Rizwan <hameexr@gmail.com>
2023-06-01 02:23:37 +0500

    solution using counter

--- Arrays/ContainsDuplicate.py
+++ Arrays/ContainsDuplicate.py
@@ -19,3 +19,12 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)
     
+# Solution using sorting
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(0, len(nums)-1):
+            if (nums[i] == nums[i+1]):
+                return True
+        return False
+    
