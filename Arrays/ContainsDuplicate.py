0b9490e2bc96c28f9d32a7cee0f6d057704bb6d6
Hameez Rizwan <hameexr@gmail.com>
2023-06-01 02:23:01 +0500

    solution using hashmap

---
+++ Arrays/ContainsDuplicate.py
@@ -0,0 +1,16 @@
+# Problem 
+# Contains Duplicate Given an integer array nums, 
+# return true if any value appears at least twice in the array, and return false if every element is distinct.
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: true
+
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashmap = {}
+        for i in range(0, len(nums)):
+            if (nums[i] in hashmap):
+                return True
+            hashmap[nums[i]] = i
+
